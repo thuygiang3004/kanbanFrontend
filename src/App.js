416cb3313d12b1b609a46f3da7836bedcd20fe20
thuygiang3004 <thuygiang3004@gmail.com>
2022-11-16 15:49:57 -0500

    added register and login feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import Boards from "./pages/Boards";
 import Board from "./pages/Board";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -21,6 +23,8 @@ function App() {
       <Routes>
         <Route path="/boards" element={<Boards />} />
         <Route path="/board/:id" element={<Board />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/Login.js
@@ -0,0 +1,115 @@
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+import { Link } from "react-router-dom";
+
+import axios from "../api/axios";
+const LOGIN_URL = "/users/login";
+
+const Login = () => {
+  const { setAuth } = useContext(AuthContext);
+  const emailRef = useRef();
+  const errRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ email, password: pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          //   withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      console.log(JSON.stringify(response));
+      const accessToken = response?.token;
+      //   const roles = response?.data?.roles;
+      setAuth({ email, accessToken });
+      setEmail("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Invalid Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit} className="login-form">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              ref={emailRef}
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an Account?
+            <br />
+            <span className="line">
+              <Link to={"/register"}>Sign Up</Link>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default Login;
