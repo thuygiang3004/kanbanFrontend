90c0ffc413678df9f9645f96abbc70b0557d70ee
thuygiang3004 <thuygiang3004@gmail.com>
2022-11-17 13:58:10 -0500

    update to display boards of only login user

--- src/pages/Boards.js
+++ src/pages/Boards.js
@@ -1,65 +1,72 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Modal.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "../api/axios";
+import AuthContext from "../context/AuthProvider";
 
-const urlGetBoards = 'http://localhost:3002/api/boards/all';
-const urlPostNewProject = 'http://localhost:3002/api/boards/';
+const urlGetBoards = "http://localhost:3002/api/boards/all";
+const urlPostNewProject = "http://localhost:3002/api/boards/";
 
 const Boards = () => {
   const [loading, setLoading] = useState(true);
   const [boards, setBoards] = useState([]);
 
   const [value, onChange] = useState(new Date());
-  const [projectTitle, setProjectTitle] = useState('');
+  const [projectTitle, setProjectTitle] = useState("");
   const [dueDate, setDueDate] = useState(new Date());
   const [modal, setModal] = useState(false);
   const [reducerValue, forceUpdate] = useReducer((x) => x + 1, 0);
   const [selectedDate, setSelectedDate] = useState(new Date());
 
-  //   const [activeColumn, setActiveColumn] = useState();
+  const { auth } = useContext(AuthContext);
+
   const toggleModal = () => {
     setModal(!modal);
   };
 
   if (modal) {
-    document.body.classList.add('active-modal');
+    document.body.classList.add("active-modal");
   } else {
-    document.body.classList.remove('active-modal');
+    document.body.classList.remove("active-modal");
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const addProjectToDB = async ({ projectData }) => {
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+      const addedResult = await axios.post(
+        urlPostNewProject,
+        JSON.stringify({
           title: projectData.title,
           dueDate: projectData.dueDate,
         }),
-      };
-      const addedResult = await fetch(urlPostNewProject, requestOptions).then(
-        (response) => response.json()
+        {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: "Bearer " + auth.accessToken,
+          },
+        }
       );
-      const newBoard = await addedResult.result;
+      const newBoard = await addedResult.data.result;
       setBoards((oldBoards) => [...oldBoards, newBoard]);
-      // .then(setLoading(false));
     };
 
     const projectData = { title: projectTitle, dueDate: value };
     addProjectToDB({ projectData });
 
     toggleModal();
-    // forceUpdate();
   };
 
   const fetchBoards = async () => {
-    const reponse = await fetch(urlGetBoards);
-    const newBoards = await reponse.json();
-    setBoards(newBoards.boards);
+    const response = await axios.get(urlGetBoards, {
+      headers: {
+        authorization: "Bearer " + auth.accessToken,
+      },
+    });
+    const newBoards = await response.data.boards;
+    setBoards(newBoards);
     setLoading(false);
   };
   useEffect(() => {
@@ -84,7 +91,7 @@ const Boards = () => {
       {boards.map((board) => {
         const { _id, title, dueDate } = board;
         return (
-          <article key={_id} style={{ border: '1px solid white' }}>
+          <article key={_id} style={{ border: "1px solid white" }}>
             <div>
               <h4>ID: {_id}</h4>
               <Link
