ddaafed50475815afae2a000bf704a0390bc3b3a
Michael Westmacott <michaelwestmacott@gmail.com>
2022-12-05 11:58:47 -0500

    Home page, other styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,35 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h3>Home</h3>
-        <Link to="/register"> Create an Account</Link>
+      <div className="container">
+        <section className="hero-section">
+          <div className="hero-text">
+            <h1>
+              Collaboration made easy, fast-track projects,<br></br> no
+              downloads
+            </h1>
+            <p>Task management tool</p>
+            <Link to="/register">
+              <button className="btn home-btn"> Sign up free</button>
+            </Link>
+          </div>
+          <div>
+            <img className="hero-img" src="hero-board.jpg" alt="Kanban"></img>
+          </div>
+        </section>
+        <section className="bottom-section">
+          <h2>What is Kanban?</h2>
+          <p>
+            EasyKan is a project management tool that gives users full
+            tranaparency of work. Improve project flow and maximize efficiency
+            by getting a visual representation of tasks on a kanban board, team
+            members can see the state of every task at any time and plan
+            accordingly.
+          </p>
+        </section>
       </div>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,10 @@
-import { useRef, useState, useEffect, useContext } from "react";
-import AuthContext from "../context/AuthProvider";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from '../context/AuthProvider';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import axios from "../api/axios";
-const LOGIN_URL = "/users/login";
+import axios from '../api/axios';
+const LOGIN_URL = '/users/login';
 
 const Login = () => {
   const { auth, setAuth } = useContext(AuthContext);
@@ -12,9 +12,9 @@ const Login = () => {
   const emailRef = useRef();
   const errRef = useRef();
 
-  const [email, setEmail] = useState("");
-  const [pwd, setPwd] = useState("");
-  const [errMsg, setErrMsg] = useState("");
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
   const [success, setSuccess] = useState(false);
 
   const navigate = useNavigate();
@@ -24,7 +24,7 @@ const Login = () => {
   // }, []);
 
   useEffect(() => {
-    setErrMsg("");
+    setErrMsg('');
   }, [email, pwd]);
 
   const handleSubmit = async (e) => {
@@ -35,7 +35,7 @@ const Login = () => {
         LOGIN_URL,
         JSON.stringify({ email, password: pwd }),
         {
-          headers: { "Content-Type": "application/json" },
+          headers: { 'Content-Type': 'application/json' },
           //   withCredentials: true,
         }
       );
@@ -44,19 +44,19 @@ const Login = () => {
       const accessToken = response?.data.token;
       //   const roles = response?.data?.roles;
       setAuth({ email, accessToken });
-      setEmail("");
-      setPwd("");
+      setEmail('');
+      setPwd('');
       setSuccess(true);
-      navigate("/boards");
+      navigate('/boards');
     } catch (err) {
       if (!err?.response) {
-        setErrMsg("No Server Response");
+        setErrMsg('No Server Response');
       } else if (err.response?.status === 400) {
-        setErrMsg("Invalid Username or Password");
+        setErrMsg('Invalid Username or Password');
       } else if (err.response?.status === 401) {
-        setErrMsg("Unauthorized");
+        setErrMsg('Unauthorized');
       } else {
-        setErrMsg("Login Failed");
+        setErrMsg('Login Failed');
       }
       errRef.current.focus();
     }
@@ -71,48 +71,55 @@ const Login = () => {
     <>
       {auth.email ? (
         <section>
-          <h3>You are logged in as {auth.email}</h3>
-          <button onClick={handleLogout}>Logout</button>
+          <div className="login-container">
+            <h3>You are logged in as {auth.email}</h3>
+            <button className="btn" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
         </section>
       ) : (
         <section>
           <p
             ref={errRef}
-            className={errMsg ? "errmsg" : "offscreen"}
+            className={errMsg ? 'errmsg' : 'offscreen'}
             aria-live="assertive"
           >
             {errMsg}
           </p>
-          <h1>Sign In</h1>
-          <form onSubmit={handleSubmit} className="login-form">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              ref={emailRef}
-              autoComplete="off"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              required
-            />
+          <div className="login-container">
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit} className="login-form">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                ref={emailRef}
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+              />
 
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPwd(e.target.value)}
-              value={pwd}
-              required
-            />
-            <button>Sign In</button>
-          </form>
-          <p>
-            Need an Account?
-            <br />
-            <span className="line">
-              <Link to={"/register"}>Sign Up</Link>
-            </span>
-          </p>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+              <button className="btn login-btn">Sign In</button>
+            </form>
+            <ul className="register-sign-in">
+              <li>Need an Account?</li>
+              <li>
+                <Link className="sign-login" to={'/register'}>
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
         </section>
       )}
     </>
